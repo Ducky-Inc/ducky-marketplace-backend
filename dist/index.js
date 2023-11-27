f323d4c6a2b923c6305a589471e2286a5986e6a2
Destin Martin <destintech@gmail.com>
2023-11-27 16:00:58 -0600

    chore: dockerfile fix

--- dist/index.js
+++ dist/index.js
@@ -4,16 +4,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const dotenv_1 = __importDefault(require("dotenv"));
+require("dotenv/config");
+const cors_1 = __importDefault(require("cors"));
+require('dotenv').config();
+// Services
+const AssetService_1 = __importDefault(require("./src/services/AssetService/AssetService"));
+// Routers
 const assetPerkRouter_1 = __importDefault(require("./src/routes/assetPerkRouter"));
-//For env File
-dotenv_1.default.config();
+const assetRouter_1 = __importDefault(require("./src/routes/assetRouter"));
+//Tables
+const AssetTableUtil_1 = __importDefault(require("./src/services/AssetService/utils/AssetTableUtil"));
+const PerkTableUtil_1 = __importDefault(require("./src/services/AssetService/utils/tables/PerkTableUtil"));
+const assetTableUtil = AssetTableUtil_1.default;
+const perkTableUtil = PerkTableUtil_1.default;
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8001;
-app.get("/", (req, res) => {
-    res.send("Welcome to Express & TypeScript Server");
-});
-app.use("/asset/perk", assetPerkRouter_1.default);
+// To allow specific origin
+app.use((0, cors_1.default)()); // Allow all origins for now
+app.options('*', (0, cors_1.default)()); // Allow all origins for now
+//   {
+//   origin: [
+//     'http://localhost:3000', // For local development
+//     `${process.env.FRONTEND_URL}`, // For production
+//   ],
+// }
+// Loading Singleton Services
+const assetService = AssetService_1.default;
+// Routes
+app.use('/asset/perk', assetPerkRouter_1.default); // (in progress, mocked request) addition of perks to assets, and (TODO) redemption of perks
+app.use('/asset', assetRouter_1.default); // returns all assets from the database
+// Start the server
 app.listen(port, () => {
     console.log(`Server is live at http://localhost:${port}`);
 });
--- dist/src/routes/assetPerkRouter.js
+++ dist/src/routes/assetPerkRouter.js
@@ -4,17 +4,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const assetPerkController_1 = require("../controllers/assetPerkController");
 const router = express_1.default.Router();
-router.post("/add", (req, res) => {
-    try {
-        const { addPerkDTO } = req.body; //get perkName, metadata (perk Description, perk Price, perk Quantity, perk Image)
-        const { assetAddress, perkName, metadata } = addPerkDTO;
-        const result = perkService.addPerk(assetAddress, perkName, metadata);
-        return res.send("Add Asset Perk");
-        // void redeem(req, res);
-    }
-    finally {
-    }
-});
+router.post('/add', assetPerkController_1.addPerk);
 exports.default = module.exports = router;
 //# sourceMappingURL=assetPerkRouter.js.map
